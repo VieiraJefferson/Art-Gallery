7ff6fb22c28c01f0be3ca7681253e9abae6d7cc3
Jeff Vieira <myjeff22vieira@gmail.com>
2025-05-20 17:41:24 +0100

    adicionando alteraçoes os titulos

--- art-gallery/src/components/Pages/Gallery.js
+++ art-gallery/src/components/Pages/Gallery.js
@@ -118,20 +118,20 @@ import axios from "axios";
 const MANUAL_DESCRIPTIONS = {
   "67c61483144fe266bc51bdc3": "Exploring the depths of imagination through light and shadow",
   "67c61c700f53cc65352d1079": "Bold colors, raw emotions, and surreal provocation",
-  "67c6e70f57cd6dd3670664a7": "Where colors play and reality bends",
-  "67c6ec7757cd6dd36706823a": "A new bloom: Embracing renewal and creative change",
-  "67c6ed8a57cd6dd367068b40": "An invitation to escape from the everyday",
+  "67c6e70f57cd6dd3670664a7": "A Glimpse into Human Nature",
+  "67c6ec7757cd6dd36706823a": "Raw, Real, and Uncomfortable",
+  "67c6ed8a57cd6dd367068b40": "An invitation to escape from the everyday - Simple Things, Subtle Stories",
   "67c6ee2957cd6dd36706906c": "Arte digital contemporânea e inovadora",
-  "67c6eeb457cd6dd3670693c8": "Bluring the lines between real and the fake in a daring artistic game",
+  "67c6eeb457cd6dd3670693c8": "Interrupting the Everyday with Small Revolutions",
 };
 
 const MANUAL_TITLES = {
   "67c61483144fe266bc51bdc3": "A Dream in Black and White",
   "67c61c700f53cc65352d1079": "Vivid Delirium",
-  "67c6e70f57cd6dd3670664a7": "The Infinite Playground",
-  "67c6ec7757cd6dd36706823a": "Ressurrection of the Essence",
-  "67c6ed8a57cd6dd367068b40": "Reality Shift",
-  "67c6ee2957cd6dd36706906c": "Urban Impressions",
+  "67c6e70f57cd6dd3670664a7": "Moments in Paint",
+  "67c6ec7757cd6dd36706823a": "Signs of the Times",
+  "67c6ed8a57cd6dd367068b40": "The Ordinary Seen Anew ",
+  "67c6ee2957cd6dd36706906c": "Art in Action",
   "67c6eeb457cd6dd3670693c8": "The Stolen Masterpiece",
 };
 
